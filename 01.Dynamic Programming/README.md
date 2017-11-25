5e638268a9f3121033b95d97bd389003053aac31
MoonHyuk <godmoon00@naver.com>
2017-11-25 22:58:29 +0900

    add some dp codes

--- 01.Dynamic Programming/README.md
+++ 01.Dynamic Programming/README.md
@@ -1,5 +1,13 @@
 # 다이나믹 프로그래밍
 
+(내용 추가 예정)
+
 ## 문제
 
-[팰린드롬?](problems/10942.cpp)
+### 난이도 하
+[팰린드롬?](https://www.acmicpc.net/problem/10942)
+- [코드](problems/10942.cpp)  
+
+[내리막길](https://www.acmicpc.net/problem/1520)  
+- [코드1](problems/1520.cpp)
+- [코드2](problems/1520(2).cpp)
---
+++ 01.Dynamic Programming/problems/1520.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int board[502][502];
+int dp[502][502];
+int xx[] = {1, -1, 0, 0};
+int yy[] = {0, 0, 1, -1};
+int n, m;
+
+int go(int i, int j)
+{
+    if (i == n && j == m) {
+        return 1;
+    }
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+    dp[i][j] = 0;
+    for (int k=0; k<4; k++) {
+        int nx = i + xx[k];
+        int ny = j + yy[k];
+        if (board[nx][ny] && board[nx][ny] < board[i][j]) {
+            dp[i][j] += go(nx, ny);
+        }
+    }
+    return dp[i][j];
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+
+    memset(dp, -1, sizeof(dp));
+    cin >> n >> m;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=m; j++) {
+            cin >> board[i][j];
+        }
+    }
+    go(1, 1);
+    cout << dp[1][1];
+}
